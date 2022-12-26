f0b8be4a90ae885fbf11d097db50d61021e22b11
Mu-Wei Chung <raymondc0302@gmail.com>
2022-12-26 13:15:07 +0800

    fix light/dark mode image path

--- script/changeTheme.js
+++ script/changeTheme.js
@@ -3,10 +3,10 @@ function changeTheme() {
     var video = document.getElementById("background-video")
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")) {
-        themeIcon.src = "/img/lightmode.png";
+        themeIcon.src = "img/lightmode.png";
         video.style.filter = "brightness(60%)"
     } else {
-        themeIcon.src = "/img/darkmode.png";
+        themeIcon.src = "img/darkmode.png";
         video.style.filter = "brightness(100%)"
     }
 }
